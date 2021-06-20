c317b32837159400d9e7860749df50ba0a439d44
Jason Muscat <jason.muscat@innovaflo.com>
2021-06-20 08:11:23 -0400

    comitting dot-net projects

---
+++ dot-net/DaprCounter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapr;
+using Dapr.Client;
+
+namespace DaprCounter
+{
+
+    class Program
+    {     
+        static async Task Main(string[] args)
+        {
+
+            var daprClient = new DaprClientBuilder().Build();
+
+            var counter = await daprClient.GetStateAsync<int>("statestore","counter");
+
+            while(true)
+            {
+                Console.WriteLine($"Counter = {counter++}");
+
+                await daprClient.SaveStateAsync("statestore","counter", counter);
+
+                await Task.Delay(1000);
+            }
+        }
+            
+    }
+
+}
